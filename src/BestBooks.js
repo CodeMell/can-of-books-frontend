914b842f9419bac0689ba63be88f341204f9bd82
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-23 18:26:54 -0500

    GET request
    GET request to your server /books

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,11 +1,21 @@
+import axios from 'axios';
 import React, {  useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
 const BestBooks = () => {
   // const [books, setBooks] = useState([]);
 
   useEffect(() => {
     /* TODO: Make a GET request to your API to fetch all the books from the database */
+    getBooks();
   }, []);
+
+  var getBooks = async() =>{
+    const API = `http://localhost:3001/books`;
+    console.log(API);
+    const res = await axios.get(API);
+    console.log(res);
+  }
 
   /* TODO: Render all the books in a Carousel */
 
