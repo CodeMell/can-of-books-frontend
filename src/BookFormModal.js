5270d9844dff1eb9fac1bd73eacd198981c69d2e
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-25 10:15:21 -0500

    Create BookFormModal.js

---
+++ src/BookFormModal.js
@@ -0,0 +1,12 @@
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+const BookFormModal = () => {
+    return (
+        <Modal>
+            
+        </Modal>
+    );
+  };
+  
+  export default BookFormModal;
