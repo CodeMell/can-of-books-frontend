842c87d27994eb556498e58b65897f30408c8313
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-06-06 18:10:33 -0500

    add auth to post route

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 
 const Footer = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
       <Navbar.Brand>Code Fellows</Navbar.Brand>
     </Navbar>
   );
